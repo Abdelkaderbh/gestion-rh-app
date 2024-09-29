1c5215b0a3d773ead7e15b7f6262959330bba65b
Majdi FEKIH SALEM <101369307+majdifkih@users.noreply.github.com>
2024-09-29 18:59:19 +0100

    SignIn&SignUp Interfaces (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SignIn from "./Pages/Auth/SignIn";
+import SignUp from "./Pages/Auth/SignUp";
 
 function App() {
-  return <></>;
+  return  (<div className="App">
+  <Routes>
+    <Route path="/login" element={<SignIn />} />
+    <Route path="/register" element={<SignUp />} />
+    
+  </Routes>
+  
+</div>
+);
 }
 
 export default App;
