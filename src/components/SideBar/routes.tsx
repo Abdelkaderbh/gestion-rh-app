b573bce34f2ce1c13eec1392db1b15ba5b7924b2
Abdelkaderbh <gaderbh2@gmail.com>
2024-10-08 18:16:36 +0100

    fix: minor error fix

--- src/components/SideBar/routes.tsx
+++ src/components/SideBar/routes.tsx
@@ -1,32 +1,20 @@
-
-import NotFound from "@/Pages/NotFound/NotFound";
-import SignIn from "../../Pages/Auth/SignIn";
-import SignUp from "../../Pages/Auth/SignUp";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
 import { RouteType } from "./SideBar";
 import Employees from "@/Pages/Employees/Employees";
 
-
 const routes: RouteType[] = [
-    {
-      path: '/dashboard',
-      icon: 'HomeIcon',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/employees',
-
-      icon: 'BadgeIcon',
-      name: 'Employees',
-      component: Employees
-    },
-
-      icon: 'DescriptionIcon', 
-      name: 'Employees',
-      component: Dashboard
-    },
-]
+  {
+    path: "/dashboard",
+    icon: "HomeIcon",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/employees",
+    icon: "BadgeIcon",
+    name: "Employees",
+    component: Employees,
+  },
+];
 
 export default routes;
-  
