f017605e24dfba8d88216ecde89d8b8c5c9eef58
Abdelkaderbh <gaderbh2@gmail.com>
2024-11-18 22:04:49 +0100

    fix: sidebar conflit fix

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { NavLink } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
@@ -13,21 +12,9 @@ import { SvgIconTypeMap } from "@mui/material/SvgIcon";
 import { SvgIconProps } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { employeeRoutes, hrRoutes } from "./routes";
-
-=======
-import React from 'react';
-import routes from './routes'
-import { NavLink } from 'react-router-dom';
-import HomeIcon from '@mui/icons-material/Home';
-import BadgeIcon from '@mui/icons-material/Badge';
-import DescriptionIcon from '@mui/icons-material/Description';
-import SidebarSubmenu from './SidebarSubmenu';
-import { SvgIconTypeMap } from '@mui/material/SvgIcon';
-import { SvgIconProps } from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-import ContactMailIcon from '@mui/icons-material/ContactMail';
+import ContactMailIcon from "@mui/icons-material/ContactMail";
 // Définir les types pour les routes
->>>>>>> 9264d5366ba16e14b9194b86455d22ec97313247
+
 export interface RouteType {
   path: string;
   icon?: string;
@@ -36,15 +23,14 @@ export interface RouteType {
   routes?: RouteType[];
 }
 
-<<<<<<< HEAD
 const iconMap: {
   [key: string]: OverridableComponent<SvgIconTypeMap<object, "svg">>;
 } = {
   HomeIcon,
   BadgeIcon,
   DescriptionIcon,
-  CalendarTodayIcon, 
-  AccessTimeIcon, 
+  CalendarTodayIcon,
+  AccessTimeIcon,
   AssessmentIcon,
   CheckCircleIcon,
 };
@@ -52,19 +38,10 @@ const iconMap: {
 interface IconProps extends SvgIconProps {
   icon: string;
 }
-=======
-const iconMap: { [key: string]: OverridableComponent<SvgIconTypeMap<object, "svg">> } = {
-    HomeIcon,
-    BadgeIcon,
-    DescriptionIcon,
-    ContactMailIcon,
-  };
-  
-  
-  interface IconProps extends SvgIconProps {
-    icon: string;
-  }
->>>>>>> 9264d5366ba16e14b9194b86455d22ec97313247
+
+interface IconProps extends SvgIconProps {
+  icon: string;
+}
 
 const Icon: React.FC<IconProps> = ({ icon, ...props }) => {
   const IconComponent = iconMap[icon];
@@ -75,30 +52,8 @@ interface SidebarProps {
   role: string;
 }
 
-<<<<<<< HEAD
 const Sidebar: React.FC<SidebarProps> = ({ role }) => {
   const routes = role === "HR" ? hrRoutes : employeeRoutes;
-=======
-const Sidebar: React.FC = () => {
-    return (
-      <aside className="z-30 flex-shrink-0 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 lg:block">
-        <div className="py-4 text-gray-500 dark:text-gray-400">
-          <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
-            RH Project
-          </a>
-          <ul className="mt-6">
-  {routes.map((route: RouteType) =>
-    route.routes ? (
-      <SidebarSubmenu route={route} key={route.name} />
-    ) : (
-      <li className="relative px-6 py-3" key={route.name}>
-        <NavLink
-          to={`/app${route.path}`}
-          className={({ isActive }) =>
-            `inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 ${
-              isActive ? ' text-purple-600 dark:text-purple-300' : ''
->>>>>>> 9264d5366ba16e14b9194b86455d22ec97313247
-
   return (
     <aside className="z-30 flex-shrink-0 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 lg:block">
       <div className="py-4 text-gray-500 dark:text-gray-400">
