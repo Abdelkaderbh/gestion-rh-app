976b57e4e96c57b6395c0355d37a8a9e20b31cbb
Abdelkaderbh <gaderbh2@gmail.com>
2024-12-27 07:19:13 +0100

    fix: leaves requests and evaluations crud

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import GroupsIcon from "@mui/icons-material/Groups";
 import PaidIcon from "@mui/icons-material/Paid";
 import SmsIcon from "@mui/icons-material/Sms";
@@ -6,6 +6,9 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PageTitle from "@/components/Titles/PageTitle";
 import Card from "@/components/Cards/Card";
 import { useAuth } from "@/hooks/useAuth";
+import { useLeave } from "@/hooks/useLeave";
+import { useEmployee } from "@/hooks/useEmployee";
+import { useTimesheet } from "@/hooks/useTimesheet";
 
 const capitalizeFirstLetter = (name: string | undefined) => {
   if (!name) return "";
@@ -16,6 +19,14 @@ const Dashboard: React.FC = () => {
   const { user } = useAuth();
   const role = user?.role;
   const userName = capitalizeFirstLetter(user?.name);
+  const {leaves} = useLeave();
+  const {employees} = useEmployee();
+  const {timesheets} = useTimesheet();
+
+  const totalLeaves = leaves ? leaves.length : 0;
+  const totalEmployees = employees ? employees.length : 0;
+  const totalTimesheets = timesheets ? timesheets.length : 0;
+ 
 
   return (
     <>
@@ -33,21 +44,21 @@ const Dashboard: React.FC = () => {
 
       {role === "HR" && (
         <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-          <Card title="Total Employees" value="100">
+          <Card title="Total Employees" value={totalLeaves}>
             <GroupsIcon
               component="svg"
               className="mr-4 text-orange-500 dark:text-orange-100 bg-orange-100 dark:bg-orange-500"
             />
           </Card>
 
-          <Card title="Conge Demands" value="18">
+          <Card title="Conge Demands" value={totalEmployees}>
             <PaidIcon
               component="svg"
               className="mr-4 text-green-500 dark:text-green-100 bg-green-100 dark:bg-green-500"
             />
           </Card>
 
-          <Card title="Timesheets demand" value="20">
+          <Card title="Timesheets demand" value={totalTimesheets}>
             <ShoppingCartIcon
               component="svg"
               className="mr-4 text-blue-500 dark:text-blue-100 bg-blue-100 dark:bg-blue-500"
